ab12e0ef29471cbe03545fdbffa2b414206cff95
Andrés Vidal <andresvidalberriel@gmail.com>
2020-02-07 12:18:54 -0300

    Add ApplicationResource

---
+++ src/main/java/br/edu/ifrs/transnacionalidades/ApplicationResource.java
@@ -0,0 +1,8 @@
+package br.edu.ifrs.transnacionalidades;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationResource extends Application {
+}
